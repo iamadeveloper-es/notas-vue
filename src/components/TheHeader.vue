9aea897bd727696780a6c4e0e626a2cdd7e03e49
Daniel Verdú <verdudaniel7@gmail.com>
2021-07-05 18:33:48 +0200

    configuration project

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="app-header">
+      <b-container fluid>
+          <b-row>
+              <b-col cols="12">
+                  <div class="grid">
+
+                  </div>
+              </b-col>
+          </b-row>
+      </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.app-header
+.grid{
+    display: grid;
+    grid-template-columns: repeat(8, 1fr);
+    gap: 20px;
+}
+</style>
